70f2342acc5c83e69ef70cbe77679b63217dddc0
sam16kazah <samkazah444@gmail.com>
2023-12-07 11:28:39 +0300

    add presist filter

--- src/app/store.js
+++ src/app/store.js
@@ -1,11 +1,31 @@
 import { combineReducers, configureStore } from '@reduxjs/toolkit';
 import userSlice from '../features/userSlice';
+import storage from 'redux-persist/lib/storage';
+import { persistReducer } from 'redux-persist/es/persistReducer';
+import persistStore from 'redux-persist/es/persistStore';
+import createFilter from 'redux-persist-transform-filter';
+
+const saveUserOnlyFilter = createFilter('user', ['user']);
+//presist config
+const persistConfig = {
+  key: 'user',
+  storage,
+  whitelist: ['user'],
+  transforms: [saveUserOnlyFilter],
+};
 
 const rootReducer = combineReducers({
   user: userSlice,
 });
 
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
 export const store = configureStore({
   reducer: rootReducer,
+  middleware: (getDefaultMiddleware) => {
+    getDefaultMiddleware({ serializableCheck: false });
+  },
   devTools: true,
 });
+
+export const persistor = persistStore(store);
