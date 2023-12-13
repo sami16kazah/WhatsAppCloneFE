8dcc0f6e6ac40da3c67d2bcc7c50ce4c81129a88
sam16kazah <samkazah444@gmail.com>
2023-12-13 13:52:07 +0300

    finish search

--- src/components/sidebar/search/Search.jsx
+++ src/components/sidebar/search/Search.jsx
@@ -31,7 +31,12 @@ export default function Search({ searchLength, setSearchResults }) {
         <div className="flex items-center gap-x-2">
           <div className="w-full flex dark:bg-dark_bg_2 rounded-lg pl-2">
             {show || searchLength > 0 ? (
-              <span className="w-8 flex items-center justify-center rotateAnimation">
+              <span
+                onClick={() => {
+                  setSearchResults([]);
+                }}
+                className="w-8 flex items-center justify-center rotateAnimation cursor-pointer"
+              >
                 <ReturnIcon className="fill-green_1 w-5"></ReturnIcon>
               </span>
             ) : (
